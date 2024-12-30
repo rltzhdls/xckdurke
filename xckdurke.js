d738e6c79f7a68fc966b38b4688a30ae5a4d647d
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-12-30 13:31:37 +0900

    Add files via upload

---
+++ xckdurke.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+
+		var ime = document.querySelector('.malo');
+        if (ime)
+        {
+        
+        ime.src = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEij-8WBQUKl1_WJN5jZFQ_0QaEfeWHnrYKekXXgD_AtylAcEELBvhxXkW06ClXd9rT6xyhtNVue64xFvYuaGhy_eE_cKspElKEML5K75gSVqbXzaIpgHDGVQurtNzbLw8Ttz-pV9t2m7ULTADWHH57EdE4caCSgIKJVbT9BELWEjZ2FprYG31FlFW_hBEJG/s16000/Royaleditabletexteffect_6d64b4c9-a3d1-46c8-9fb6-07d2fb9e2878.png';
+        ime.style.width = '550px';
+		}
+    
+	}
+});
+
